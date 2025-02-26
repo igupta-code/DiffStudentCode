0982ada23745a93f0f860ea47374266b07f69e91
ishagupta <isha.gupta@menloschool.org>
2025-02-26 09:54:24 -0800

    First

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 /**
  * Plagiarism Checker
  * A tool for finding the longest shared substring between two documents.
@@ -15,19 +17,29 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-        int[][] table = new int[doc1.length() + 1][doc2.length() + 1];
+        int[][] table = new int[doc1.length()+1][doc2.length()+1];
+        int maxL = 0;
 
-        for(int i = 0; i < doc1.length(); i++){
-            for(int j = 0; j < doc2.length(); j++){
+        for(int i = 1; i < doc1.length()+1; i++){
+            for(int j = 1; j < doc2.length()+1; j++){
+                table[i][j] = table[i][j-1];
+
                 // Look for letter you're adding in the other string (if you can't find it move on)
+                char c = doc2.charAt(j-1);
 
-                // Look at the square that corresponds to the smaller problem and add one
+                // Go through other array looking for c
+                for(int k = i; k > 0; k--){
+                    if(doc1.charAt(k-1) == c){
+                        // Look at the square that corresponds to the smaller problem and add one
+                        maxL = Math.max(table[k - 1][j - 1]+1, table[i][j]);
+                        table[i][j] = maxL;
+                        break;
+                    }
+                }
 
                 // Check previous highest number to make sure that's not bigger + update
             }
         }
-
-
-        return 0;
+        return maxL;
     }
 }
